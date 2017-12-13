99a2d3e95bba88bd4e0033c3530d2251170a4431
g.andrianov <g.andrianov@tinkoff.ru>
2017-12-13 15:49:59 +0300

    Change CookieSet test

--- src/test/scala/CookiesSetTest.scala
+++ src/test/scala/CookiesSetTest.scala
@@ -4,34 +4,34 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.util.Random
 
 class CookiesSetTest extends FlatSpec with Matchers {
-//  trait mocks{
-//    val emptyCookieSet = CookiesSet(Map[String, BigDecimal]())
-//    val randomValue:BigDecimal = Random.nextInt()
-//    val randomStr = Random.nextString(10)
-//
-//  }
-//
-//
-//  "add" should "add elements that are not contained" in new mocks{
-//      emptyCookieSet.add(randomStr, randomValue) should be(CookiesSet(Map[String, BigDecimal](randomStr -> randomValue)))
-//  }
-//
-//  "contains" should "return true if element is in cookieSet" in new mocks{
-//    emptyCookieSet.add(randomStr, randomValue).contains(randomStr) should be(true)
-//  }
-//  it should "return false if element is not in cookieSet" in new mocks{
-//    emptyCookieSet.add(randomStr, randomValue).contains(randomStr.concat(randomStr)) should be(false)
-//  }
-//
-//  "getId" should "return id if element is in cookieSet" in new mocks{
-//    emptyCookieSet.add(randomStr, randomValue).getId(randomStr) should be(randomValue)
-//  }
-//
-//  it should "return -1 on not contained element" in new mocks {
-//    emptyCookieSet.getId(randomStr) should be(-1)
-//  }
-//
-//  "delete" should "delete contained element" in new mocks {
-//    emptyCookieSet.add(randomStr, randomValue).delete(randomStr).contains(randomStr) should be(false)
-//  }
+  trait mocks{
+    val emptyCookieSet = new CookiesSet(Map[String, BigDecimal]())
+    val randomValue:BigDecimal = Random.nextInt()
+    val randomStr = Random.nextString(10)
+
+  }
+
+  "contains" should "return true if element is in cookieSet" in new mocks{
+    emptyCookieSet.add(randomStr, randomValue)
+    emptyCookieSet.contains(randomStr) should be(true)
+  }
+  it should "return false if element is not in cookieSet" in new mocks{
+    emptyCookieSet.add(randomStr, randomValue)
+    emptyCookieSet.contains(randomStr.concat(randomStr)) should be(false)
+  }
+
+  "getId" should "return id if element is in cookieSet" in new mocks{
+    emptyCookieSet.add(randomStr, randomValue)
+    emptyCookieSet.getId(randomStr) should be(randomValue)
+  }
+
+  it should "return -1 on not contained element" in new mocks {
+    emptyCookieSet.getId(randomStr) should be(-1)
+  }
+
+  "delete" should "delete contained element" in new mocks {
+    emptyCookieSet.add(randomStr, randomValue)
+    emptyCookieSet.delete(randomStr)
+    emptyCookieSet.contains(randomStr) should be(false)
+  }
 }
